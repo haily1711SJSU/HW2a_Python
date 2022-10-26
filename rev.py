bb56e060816af6d449fbe808a5e40964a40f407a
Hai Ly SJSU <haily2221@gmail.com>
2022-10-25 22:32:53 -0700

    remove

--- rev.py
+++ rev.py
@@ -1,14 +1,7 @@
-
 def reverse_list(array):
     reversed_list = []
     for i in reversed(array):
         reversed_list.append(i)
     return reversed_list
 
-#for testing 
-def main():
-    nums = [1, True, 9, 'Hi', 3]
-    print(reverse_list(nums))
-
-main()
 
