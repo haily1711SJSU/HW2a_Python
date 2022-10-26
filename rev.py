fa697fd463dd7f226c66e60985899e294bc50e75
Hai Ly SJSU <102846798+haily1711SJSU@users.noreply.github.com>
2022-10-25 22:31:53 -0700

    Update rev.py

--- rev.py
+++ rev.py
@@ -5,10 +5,3 @@ def reverse_list(array):
         reversed_list.append(i)
     return reversed_list
 
-#for testing 
-def main():
-    nums = [1, True, 9, 'Hi', 3]
-    print(reverse_list(nums))
-
-main()
-
