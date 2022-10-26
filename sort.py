f21823ee7373f669b46ef3bdc15c746c2bcd549e
Hai Ly SJSU <haily2221@gmail.com>
2022-10-25 23:46:21 -0700

    Optimized

--- sort.py
+++ sort.py
@@ -1,8 +1,5 @@
 from operator import itemgetter
 def sort_dictionary(dict):
-    # sort dictionary by age (second element of each tuple value)
-    # and generate a sorted list
-    sorted_dict = sorted(dict.items(), key=lambda item: item[1])
     tuples = []
     for key,value in dict.items():
         tuples.append((key,value[0],value[1]))  
