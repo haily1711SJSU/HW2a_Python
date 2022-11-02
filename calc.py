5c68409989af1caef92524f54ef2665e72b1949d
Hai Ly SJSU <haily2221@gmail.com>
2022-11-02 00:45:07 -0700

    Pemdas

--- calc.py
+++ calc.py
@@ -1,7 +1,7 @@
 
 def calculator(input):
     operators = ["+","-","*","/","//","(",")"]
-    math_Operators = set(operators)
+    math_operators = set(operators)
     operators.clear() # save space
     # remove all white spaces, and make sure
     # input is converted to string to avoid input errors
@@ -11,46 +11,54 @@ def calculator(input):
     for s in expression:
         if s.isdecimal():
             continue
-        if s not in math_Operators:
+        if s not in math_operators:
             print(f'Expression {expression} contains an invalid operation {s}')
             return
-    math_Operators.clear() # save space
-    parenthesis = [] 
-    ans = []
-    # parenthesis prioritized first
-    stack = [] # helps with dealing with parenthesis
-    if "(" in expression:
-        for s,i in enumerate(expression):
-            if s == "(":
-                stack.append(s) #push into stack
-            elif s == ")": #append the expression inside
-                        # the parenthesis into a queue as a list
-                        # and reset the stack
-                parenthesis.append(stack[1::])
-                stack.clear()
-            elif "(" in stack: #push numbers and operations
-                            # in between parenthesis
-                stack.append(s)
+    input_list = []
+    i = 0
+    while i < len(expression):
+        digit = ""
+        while expression[i].isdigit() and i < len(expression)-1:
+            digit += expression[i]
+            i += 1
+        input_list.append(digit)
+        if expression[i] in math_operators:
+            if expression[i]+expression[i+1] == "**":
+                input_list.append(expression[i]+expression[i+1])
+                i += 1
+            elif expression[i]+expression[i+1] == "//":
+                input_list.append(expression[i]+expression[i+1])
+                i += 1
+            else:
+                input_list.append(expression[i])
+        i += 1
         
-        # evaluate expression in parenthesis
-        
-    print(parenthesis)
+    input_list[-1] += expression[-1]
     op_func = {"**": expo, "*":mul, "/": div, "//": divInt,
             "+": add, "-":sub}
-    #print(evaluate(parenthesis[0],op_func))
 
-    return expression
+    return evaluate(input_list, op_func)
 
 def evaluate(input, op_func):
     queue = ["**","*","/","//","+","-"]
     q = []
+    print(input)
     for op in queue:
-        for i in range(len(input)):
+        n = len(input)
+        i = 0
+        while i < n:
             if op == input[i]:
-                ans = op_func[op](int(input[i-1]),int(input[i+1]))
-                input[i-1] = ans
+                left = int(input[i-1])
+                right = int(input[i+1])
+                ans = op_func[op](left,right)
                 q.append(ans)
-    return q
+                for a in range(2):
+                    input.pop(i)
+                input.pop(i-1)
+                input.insert(i-1, ans)
+            n = len(input)
+            i += 1
+    return float(q[-1])
 
 def expo(a, b):
     return a ** b
@@ -66,6 +74,4 @@ def sub(a, b):
     return a - b
 
 
-    
-
-print(calculator("(5+3*5**3)"))
+print(calculator("11/31"))
